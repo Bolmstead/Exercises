649b7de45382afbbb05db63d17cc2c4d15fef77e
Berkley Olmstead <olms2074@gmail.com>
2021-01-06 14:52:10 -0700

    eight-ball complete

---
+++ eight-ball/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import Eightball, { answers } from "./Eightball";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Eightball />
+    </div>
+  );
+}
+
+export default App;
